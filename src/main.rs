577dbe848165882f20f2564fc2c5becd5debd5b1
Anton Österberg <anton.osterberg@gmail.com>
2019-11-06 23:56:09 +0100

    Parsing token and making request

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,45 @@ use std::fs;
 const GITHUB_API: &str = "https://api.github.com";
 
 fn main() {
-    let file_content: String = fs::read_to_string(".git/config").expect("Could not find a git config");
-    let lines: Vec<&str> = file_content.lines()
+    let file_content: String =
+        fs::read_to_string(".git/config").expect("Could not find a git config");
+
+    let args: Vec<String> = env::args().collect();
+    let token: Option<&String> = args
+        .iter()
+        .skip_while(|i| !i.contains("--token"))
+        .skip(1)
+        .next();
+
+    let token: String = match token {
+        Some(t) => t.clone(),
+        None => env!("GITHUB_TOKEN").to_string(),
+    };
+
+    println!("{}", token);
+
+    //.skip_while(|i| !i.contains("--token"));
+
+    let lines: Vec<&str> = file_content
+        .lines()
         .filter(|f| f.contains("github.com"))
         .collect();
 
     lines.iter().for_each(|f| println!("{:?}", f));
 
-    let repo: &str = lines.first()
+    let repo: &str = lines
+        .first()
         .expect("No Github repoistory found")
         .split_terminator(":")
         .last()
         .expect("No match");
 
+    let repo: &str = repo.trim_end_matches(".git");
+
     println!("{}", repo);
-    // let url = [GITHUB_API, "repos", owner, repo, "issues"].join("/");
-    // let body = reqwest::get(url)
-    //     .await()?
-    //     .text()
-    //     .await()?;
+
+    let url: String = [GITHUB_API, "repos", repo, "issues"].join("/");
+    let body = reqwest::get(&url).expect("Request failed");
+
+    println!("{:?}", body);
 }
