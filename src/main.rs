2a39ed5aec76a25a38f372424715fb06b303387a
Anton Österberg <anton.osterberg@gmail.com>
2019-11-07 22:03:31 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,21 @@ use std::fs;
 const GITHUB_API: &str = "https://api.github.com";
 
 fn main() {
-    let file_content: String =
-        fs::read_to_string(".git/config").expect("Could not find a git config");
+    let token: String = read_token();
+    println!("{}", token);
 
+    let repo: String = read_repo();
+    println!("{}", repo);
+
+    println!("has add: {}", cmd_has("add"));
+    if cmd_has("add") {
+        create_issue(&repo, &token)
+    } else {
+        list_issues(&repo, &token)
+    }
+}
+
+fn read_token() -> String {
     let args: Vec<String> = env::args().collect();
     let token: Option<&String> = args
         .iter()
@@ -16,21 +28,20 @@ fn main() {
         .skip(1)
         .next();
 
-    let token: String = match token {
+    match token {
         Some(t) => t.clone(),
         None => env!("GITHUB_TOKEN").to_string(),
-    };
+    }
+}
 
-    println!("{}", token);
-
-    //.skip_while(|i| !i.contains("--token"));
+fn read_repo() -> String {
+    let file_content: String =
+        fs::read_to_string(".git/config").expect("Could not find a git config");
 
     let lines: Vec<&str> = file_content
         .lines()
         .filter(|f| f.contains("github.com"))
         .collect();
-
-    lines.iter().for_each(|f| println!("{:?}", f));
 
     let repo: &str = lines
         .first()
@@ -39,10 +50,14 @@ fn main() {
         .last()
         .expect("No match");
 
-    let repo: &str = repo.trim_end_matches(".git");
+    repo.trim_end_matches(".git").to_string()
+}
 
-    println!("{}", repo);
+fn cmd_has(key: &str) -> bool {
+    env::args().any(|i| i == key)
+}
 
+fn list_issues(repo: &String, token: &String) {
     let url: String = [GITHUB_API, "repos", repo, "issues"].join("/");
     let client = reqwest::Client::new();
     let mut response: reqwest::Response = client
@@ -55,6 +70,8 @@ fn main() {
 
     println!("{:?}", body);
 }
+
+fn create_issue(repo: &String, token: &String) {}
 
 #[derive(Debug, Deserialize)]
 struct Issue {
