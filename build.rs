f55869d5cda4329d055ff62d0b4c0c146676377d
Anton Österberg <anton.osterberg@protonmail.com>
2023-03-26 13:00:10 +0200

    wip: Do not use "built" as build time dependency

--- build.rs
+++ build.rs
@@ -1,19 +1,8 @@
-use built::Options;
-use std::path::{Path, PathBuf};
-
 fn main() {
-    let mut default: Options = Options::default();
-    let options = default
-        .set_compiler(true)
-        .set_cfg(true)
-        .set_ci(false)
-        .set_dependencies(false)
-        .set_git(true)
-        .set_env(true)
-        .set_features(true);
+    // Fetch git tag/commit with `git describe --all --always --long --broken --dirty`
+    for (key, value) in std::env::vars().into_iter().filter(|(k, _)| k.starts_with("CARGO")) {
+        println!("{key}: {value}");
+    }
 
-    let src: PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    let dst: PathBuf = Path::new(&std::env::var("OUT_DIR").unwrap()).join("built.rs");
-
-    built::write_built_file_with_opts(&options, &src, &dst).expect("Failed to acquire build-time information");
+    std::fs::write("target/build_data", "foo");
 }
